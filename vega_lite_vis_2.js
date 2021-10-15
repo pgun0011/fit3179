a8cd6b565c9e917ac2fb17009759b7c809ef5dd7
pgun0011 <69449953+pgun0011@users.noreply.github.com>
2021-10-15 17:05:24 +1100

    Add files via upload

--- vega_lite_vis_2.js
+++ vega_lite_vis_2.js
@@ -1,4 +1,4 @@
-var vg_2 = "F1_winners.json";
+var vg_2 = "F1_winners";
 vegaEmbed("#bar_chart", vg_2).then(function(result) {
  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
