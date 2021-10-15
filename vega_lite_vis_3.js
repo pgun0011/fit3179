1c2332846586a699eed8d9752cb5f619a9f5e894
pgun0011 <69449953+pgun0011@users.noreply.github.com>
2021-10-15 17:23:13 +1100

    Add files via upload

---
+++ vega_lite_vis_3.js
@@ -0,0 +1,4 @@
+var vg_3 = "F1_points.json";
+vegaEmbed("#pie_chart", vg_3).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
